63b9fe6b7cd00cab2029251541d85d02e43ebac9
Bar Tzipori <bar.tzipori@gmail.com>
2024-06-20 21:03:15 +0300

    added db search functionality

--- backend/db.js
+++ backend/db.js
@@ -32,4 +32,30 @@ async function insertIntoDb(data) {
   }
 }
 
+async function getDocumentsWithSharedTags(caseid) {
+  try {
+    await client.connect();
+
+    const query = `
+    SELECT ct1.case_id AS similar_case_id, c.id, c.content, COUNT(*) AS shared_tags,
+           array_agg(t.name) AS shared_tag_names
+    FROM case_tags ct1
+    JOIN case_tags ct2 ON ct1.tag_id = ct2.tag_id
+    JOIN cases c ON ct1.case_id = c.id
+    JOIN tags t ON ct1.tag_id = t.id
+    WHERE ct2.case_id = $1
+      AND ct1.case_id != $1
+    GROUP BY ct1.case_id, c.content
+    ORDER BY shared_tags DESC;
+  `;
+    const values = [caseid];
+    const result = await client.query(query, values);
+    return result.rows;
+  } catch (err) {
+    console.error('Error executing query', err.stack);
+  } finally {
+    await client.end();
+  }
+}
+
 module.exports = { getFromDb, insertIntoDb };
